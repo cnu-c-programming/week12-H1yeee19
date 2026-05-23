0df7e568aa6e5933a6a43dd69f06532165cc9b19
H1yeee19 <happylacu@gmail.com>
2026-05-23 14:05:12 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -11,7 +11,7 @@ typedef struct config {
 
 void config_parser(Config* config_ptr) {
     FILE *fp = fopen("config.txt", "r");
-    fscanf(fp, "InputFileName=%s Options=%d SectionName=%s Address=%llu", config_ptr->InputFileName, &config_ptr->Options, config_ptr->SectionName, &config_ptr->Address);
+    fscanf(fp, "InputFileName=%s Options=%d SectionName=%s Address=%lli", config_ptr->InputFileName, &config_ptr->Options, config_ptr->SectionName, &config_ptr->Address);
     fclose(fp);
 }
 
@@ -19,7 +19,7 @@ int main(int argc, const char* argv[]) {
     Config config;
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
+    printf("config: %s %d %s %lli\n", 
         config.InputFileName, 
         config.Options,
         config.SectionName,
