fc750a7bfd20c51648809f769edbe69a80759703
H1yeee19 <happylacu@gmail.com>
2026-05-23 16:43:20 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -3,7 +3,17 @@
 int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
+FILE *fp_src = fopen(argv[1], "r");
+    FILE *fp_dst = fopen(argv[2], "w");
 
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        fputc(c, fp_dst);   // 읽은 문자 c를 fp_dst에 작성(복사)
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
+    return 0;
 
 }
 
